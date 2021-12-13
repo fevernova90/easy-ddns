88d0ea0a17194fb75e7c544122095d7f3e23b9e6
Muhaimin Taib <muhaimin@gotelz.com>
2021-12-13 18:36:29 +0800

    Multiarch docker build

--- push-image.sh
+++ push-image.sh
@@ -8,14 +8,23 @@ REMOTE_REPO=fevernova90
 COMMIT_SHORT_HASH=$(git log -1 --pretty=%h)
 
 # Build
-docker build \
+# docker build \
+#   --target production \
+#   -t $REMOTE_REPO/$IMAGE_NAME:$VERSION \
+#   -t $REMOTE_REPO/$IMAGE_NAME:$COMMIT_SHORT_HASH \
+#   -t $REMOTE_REPO/$IMAGE_NAME:latest \
+#   --build-arg NODE_ENV=$TAG .
+
+# Multi-arch build
+docker buildx build \
+  --platform linux/amd64,linux/arm64,linux/arm/v7 \
   --target production \
   -t $REMOTE_REPO/$IMAGE_NAME:$VERSION \
   -t $REMOTE_REPO/$IMAGE_NAME:$COMMIT_SHORT_HASH \
   -t $REMOTE_REPO/$IMAGE_NAME:latest \
-  --build-arg NODE_ENV=$TAG .
+  --build-arg NODE_ENV=$TAG --push .
 
 # Push
-docker push $REMOTE_REPO/$IMAGE_NAME:$COMMIT_SHORT_HASH
-docker push $REMOTE_REPO/$IMAGE_NAME:$VERSION
-docker push $REMOTE_REPO/$IMAGE_NAME:latest
+# docker push $REMOTE_REPO/$IMAGE_NAME:$COMMIT_SHORT_HASH
+# docker push $REMOTE_REPO/$IMAGE_NAME:$VERSION
+# docker push $REMOTE_REPO/$IMAGE_NAME:latest
